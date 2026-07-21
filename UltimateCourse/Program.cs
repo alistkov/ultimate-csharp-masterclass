682c495c7d10450fa08a5de730c1996a51f41519
alistkov <alistkov.dev@gmail.com>
2026-07-21 23:21:25 +0200

    video 37

--- UltimateCourse/Program.cs
+++ UltimateCourse/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Provide a number");
+string userInput = Console.ReadLine();
+int number = int.Parse(userInput);
+Console.WriteLine(number);
+
+// Console.WriteLine("Hello!");
+// Console.WriteLine("What do you want to do?");
+// Console.WriteLine("[S]ee all TODOs");
+// Console.WriteLine("[A]dd a TODO");
+// Console.WriteLine("[R]emove a TODO");
+// Console.WriteLine("[E]xit");
+//
+// var userChoice = Console.ReadLine();
+//
+// switch (userChoice)
+// {
+//     case "S":
+//         PrintSelectedOption("See all TODOs");
+//         break;
+//     case "A":
+//         PrintSelectedOption("Add a TODO");
+//         break;
+//     case "R":
+//         PrintSelectedOption("Remove a TODO");
+//         break;
+//     case "E":
+//         PrintSelectedOption("Exit");
+//         break;
+// }
+//
+// void PrintSelectedOption(string selectedOption)
+// {
+//     Console.WriteLine($"Selected option: {selectedOption}");
+// }
+//
+// Console.ReadKey();
