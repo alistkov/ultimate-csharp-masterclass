2a7b04474de302d2a4bc5c7960a4cea8e735bfa5
alistkov <alistkov.dev@gmail.com>
2026-07-21 23:46:47 +0200

    video 40

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine("Hello!");
+Console.Write("Enter the first number: ");
+var first = int.Parse(Console.ReadLine());
+Console.Write("Enter the second number: ");
+var second = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("What you want to do?");
+Console.WriteLine("[A]dd numbers");
+Console.WriteLine("[S]ubtract numbers");
+Console.WriteLine("[M]ultiply numbers");
+
+var operation = Console.ReadLine();
+Console.WriteLine();
+
+if (string.Equals(operation, "A", StringComparison.OrdinalIgnoreCase))
+{
+    Console.WriteLine($"{first} + {second} = {first + second}");
+}
+else if (string.Equals(operation, "S", StringComparison.OrdinalIgnoreCase))
+{
+    Console.WriteLine($"{first} - {second} = {first - second}");
+}
+else if (string.Equals(operation, "M", StringComparison.OrdinalIgnoreCase))
+{
+    Console.WriteLine($"{first} * {second} = {first * second}");
+}
