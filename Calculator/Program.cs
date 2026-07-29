91b0294a4a841dd23d6ed2bb38d6f5ad8f2751be
alistkov <alistkov.dev@gmail.com>
2026-07-29 23:10:51 +0200

    video 49

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,27 +1,17 @@
-﻿// char ConvertPointsToGrade(int points)
-// {
-//     return points switch
-//     {
-//         10 or 9 => 'A',
-//         8 or 7 or 6 => 'B',
-//         5 or 4 or 3 => 'C',
-//         2 or 1 => 'D',
-//         0 => 'E',
-//         _ => '!'
-//     };
-// }
-//
-// Console.WriteLine(ConvertPointsToGrade(10));
-// Console.WriteLine(ConvertPointsToGrade(3));
-// Console.WriteLine(ConvertPointsToGrade(7));
-// Console.WriteLine(ConvertPointsToGrade(110));
-
-var number = 0;
-
-while (number < 10)
+﻿char ConvertPointsToGrade(int points)
 {
-    number += 1;
-    Console.WriteLine($"Number is: {number}");
+    return points switch
+    {
+        10 or 9 => 'A',
+        8 or 7 or 6 => 'B',
+        5 or 4 or 3 => 'C',
+        2 or 1 => 'D',
+        0 => 'E',
+        _ => '!'
+    };
 }
 
-Console.WriteLine("The loop is finished");
+Console.WriteLine(ConvertPointsToGrade(10));
+Console.WriteLine(ConvertPointsToGrade(3));
+Console.WriteLine(ConvertPointsToGrade(7));
+Console.WriteLine(ConvertPointsToGrade(110));
---
+++ Theory/Program.cs
@@ -0,0 +1,8 @@
+﻿string word;
+
+do
+{
+    Console.WriteLine("Enter a word longer than 10 letters");
+    word = Console.ReadLine();
+    
+} while (word!.Length <= 10);
