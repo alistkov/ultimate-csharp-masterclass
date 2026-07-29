aa6b905c5d83cd0631a1be20c4e38896cd787b1a
alistkov <alistkov.dev@gmail.com>
2026-07-29 22:58:52 +0200

    video 46

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,17 +1,27 @@
-﻿char ConvertPointsToGrade(int points)
+﻿// char ConvertPointsToGrade(int points)
+// {
+//     return points switch
+//     {
+//         10 or 9 => 'A',
+//         8 or 7 or 6 => 'B',
+//         5 or 4 or 3 => 'C',
+//         2 or 1 => 'D',
+//         0 => 'E',
+//         _ => '!'
+//     };
+// }
+//
+// Console.WriteLine(ConvertPointsToGrade(10));
+// Console.WriteLine(ConvertPointsToGrade(3));
+// Console.WriteLine(ConvertPointsToGrade(7));
+// Console.WriteLine(ConvertPointsToGrade(110));
+
+var number = 0;
+
+while (number < 10)
 {
-    return points switch
-    {
-        10 or 9 => 'A',
-        8 or 7 or 6 => 'B',
-        5 or 4 or 3 => 'C',
-        2 or 1 => 'D',
-        0 => 'E',
-        _ => '!'
-    };
+    number += 1;
+    Console.WriteLine($"Number is: {number}");
 }
 
-Console.WriteLine(ConvertPointsToGrade(10));
-Console.WriteLine(ConvertPointsToGrade(3));
-Console.WriteLine(ConvertPointsToGrade(7));
-Console.WriteLine(ConvertPointsToGrade(110));
+Console.WriteLine("The loop is finished");
