9ae9ec4a607f8c622fb625729a94fb4059aee313
alistkov <alistkov.dev@gmail.com>
2026-07-29 23:56:52 +0200

    practice

--- Theory/Program.cs
+++ Theory/Program.cs
@@ -1,4 +1,30 @@
-﻿for (var i = 0; i < 5; i += 1)
+﻿string BuildHelloString()
 {
-    Console.WriteLine("Hello!");
+    char[] letters = new[] { 'h', 'e', 'l', 'l', 'o' };
+    var result = "";
+    for(int i = 0; i < letters.Length; ++i)
+    {
+        //your code goes here
+        result = $"{result}{letters[i]}";
+    }
+    return result;
 }
+
+Console.WriteLine(BuildHelloString());
+
+bool IsWordPresentInCollection(string[] words, string wordToBeChecked)
+{
+    //your code goes here
+    for (int i = 0; i < words.Length; i++)
+    {
+        if (words[i] == wordToBeChecked)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+Console.WriteLine(IsWordPresentInCollection(["one", "two", "three"], "hello"));
+Console.WriteLine(IsWordPresentInCollection(["one", "two", "three"], "two"));
