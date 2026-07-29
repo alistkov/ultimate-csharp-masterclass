488ab683fb2941ad9ae63eab38ba0f208b999456
alistkov <alistkov.dev@gmail.com>
2026-07-29 22:55:22 +0200

    video 43

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,26 +1,17 @@
-﻿Console.WriteLine("Hello!");
-Console.Write("Enter the first number: ");
-var first = int.Parse(Console.ReadLine());
-Console.Write("Enter the second number: ");
-var second = int.Parse(Console.ReadLine());
-
-Console.WriteLine("What you want to do?");
-Console.WriteLine("[A]dd numbers");
-Console.WriteLine("[S]ubtract numbers");
-Console.WriteLine("[M]ultiply numbers");
-
-var operation = Console.ReadLine();
-Console.WriteLine();
-
-if (string.Equals(operation, "A", StringComparison.OrdinalIgnoreCase))
+﻿char ConvertPointsToGrade(int points)
 {
-    Console.WriteLine($"{first} + {second} = {first + second}");
+    return points switch
+    {
+        10 or 9 => 'A',
+        8 or 7 or 6 => 'B',
+        5 or 4 or 3 => 'C',
+        2 or 1 => 'D',
+        0 => 'E',
+        _ => '!'
+    };
 }
-else if (string.Equals(operation, "S", StringComparison.OrdinalIgnoreCase))
-{
-    Console.WriteLine($"{first} - {second} = {first - second}");
-}
-else if (string.Equals(operation, "M", StringComparison.OrdinalIgnoreCase))
-{
-    Console.WriteLine($"{first} * {second} = {first * second}");
-}
+
+Console.WriteLine(ConvertPointsToGrade(10));
+Console.WriteLine(ConvertPointsToGrade(3));
+Console.WriteLine(ConvertPointsToGrade(7));
+Console.WriteLine(ConvertPointsToGrade(110));
