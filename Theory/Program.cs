ce29a73758375f2c31908250ae74d29e6da7df07
alistkov <alistkov.dev@gmail.com>
2026-07-30 00:23:48 +0200

    practice

--- Theory/Program.cs
+++ Theory/Program.cs
@@ -1,6 +1,15 @@
-﻿var words = new [] {"one", "two", "three", "four"};
-
-foreach (var word in words)
+﻿bool IsAnyWordLongerThan(int length, string[] words)
 {
-    Console.WriteLine(word);
+    //your code goes here
+    foreach (var word in words)
+    {
+        if (word.Length > length)
+            return true;
+    }
+
+    return false;
 }
+
+Console.WriteLine(IsAnyWordLongerThan(4, ["aaa", "bbbb", "ccccc"]));
+Console.WriteLine(IsAnyWordLongerThan(5, ["aaa", "bbbb", "ccccc"]));
+Console.WriteLine(IsAnyWordLongerThan(4, []));
