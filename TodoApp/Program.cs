f8e22df9e84840f907b65efec1a066e8106a5f1c
alistkov <alistkov.dev@gmail.com>
2026-08-18 23:30:29 +0200

    video 64

--- TodoApp/Program.cs
+++ TodoApp/Program.cs
@@ -17,7 +17,7 @@ while (!shouldExit)
     {
         case "S":
         case "s":
-            Console.WriteLine("See all TODOs");
+            SeeAllTodos();
             break;
         case "A":
         case "a":
@@ -25,7 +25,7 @@ while (!shouldExit)
             break;
         case "R":
         case "r":
-            Console.WriteLine("Remove a TODO");
+            RemoveTodo();
             break;
         case "E":
         case "e":
@@ -61,4 +61,55 @@ void AddTodo()
             isValidDescription = true;
         }
     }
+}
+
+void SeeAllTodos()
+{
+    if (todos.Count == 0)
+    {
+        Console.WriteLine("No TODOs have been added yet.");
+        return;
+    }
+    for (var i = 0; i < todos.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {todos[i]}");
+    }
+}
+
+void RemoveTodo()
+{
+    if (todos.Count == 0)
+    {
+        Console.WriteLine("No TODOs have been added yet.");
+        return;
+    }
+
+    bool isIndexValid = false;
+    
+    Console.WriteLine("Select the index of the TODO you want to remove");
+    SeeAllTodos();
+
+    while (!isIndexValid)
+    {
+        var userInput = Console.ReadLine();
+
+        if (userInput == "")
+        {
+            Console.WriteLine("Selected index cannot be empty");
+        }
+
+        if (int.TryParse(userInput, out int index) && index >= 1 && index <= todos.Count)
+        {
+            var indexOfTodo = index - 1;
+            var todoToBeRemoved = todos[indexOfTodo];
+            todos.RemoveAt(indexOfTodo);
+            isIndexValid = true;
+            Console.WriteLine($"TODO removed: {todoToBeRemoved}");
+        }
+        else
+        {
+            Console.WriteLine("The given index is not valid");
+        }
+    }
+    
 }
