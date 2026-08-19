0684df6a5ac3bb3a90eea6a25ce369a24e5649bb
alistkov <alistkov.dev@gmail.com>
2026-08-19 23:46:42 +0200

    video 86

--- BasicObjectOrientedProgramming/Program.cs
+++ BasicObjectOrientedProgramming/Program.cs
@@ -1,8 +1,11 @@
 ﻿using BasicObjectOrientedProgramming;
 
-var order = new Order("Tea", new DateTime(2026, 12, 12));
-Console.WriteLine(order.Item);
-Console.WriteLine(order.Date);
+// var person = new Person("John Doe", 2000);
 
-order.Date = new DateTime(2026, 11, 11);
-Console.WriteLine(order.Date);
+var person2 = new Person
+{
+    Name = "Sam",
+    YearOfBirth = 1988
+};
+
+person2.Name = "John";
