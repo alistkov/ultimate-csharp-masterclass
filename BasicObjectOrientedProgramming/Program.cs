e304b5ebf58f5abbe3be10bad0c8ff0a8fe734db
alistkov <alistkov.dev@gmail.com>
2026-08-19 13:24:48 +0200

    video 69

---
+++ BasicObjectOrientedProgramming/Program.cs
@@ -0,0 +1,9 @@
+﻿var internationalPizzaDay = new DateTime(2026, 2, 9);
+
+Console.WriteLine($"Year is {internationalPizzaDay.Year}");
+Console.WriteLine($"Month is {internationalPizzaDay.Month}");
+Console.WriteLine($"Day is {internationalPizzaDay.Day}");
+Console.WriteLine($"Day of the week is {internationalPizzaDay.DayOfWeek}");
+
+var nextYearInternationalPizzaDay = internationalPizzaDay.AddYears(1);
+Console.WriteLine($"Day of the week is {nextYearInternationalPizzaDay.DayOfWeek}");
