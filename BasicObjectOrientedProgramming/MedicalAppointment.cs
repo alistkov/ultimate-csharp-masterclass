215f92bebe1e8083b524672cd0204f41e9729c01
alistkov <alistkov.dev@gmail.com>
2026-08-19 21:05:15 +0200

    video 77

---
+++ BasicObjectOrientedProgramming/MedicalAppointment.cs
@@ -0,0 +1,28 @@
+namespace BasicObjectOrientedProgramming;
+
+public class MedicalAppointment
+{
+    private string _patientName;
+    private DateTime _date;
+
+    public MedicalAppointment(string patientName, DateTime date)
+    {
+        _patientName = patientName;
+        _date = date;
+    }
+    
+    public void Reschedule(DateTime date)
+    {
+        _date = date;
+    }
+
+    public void OverwriteMonthAndDay(int month, int day)
+    {
+        _date = new DateTime(_date.Year, month, day);
+    }
+
+    public void MoveByMonthAndDays(int monthsToAdd, int daysToAdd)
+    {
+        _date = new DateTime(_date.Year, _date.Month + monthsToAdd, _date.Day + daysToAdd);
+    }
+}
--- BasicObjectOrientedProgramming/Program.cs
+++ BasicObjectOrientedProgramming/Program.cs
@@ -1,19 +1,6 @@
 ﻿using BasicObjectOrientedProgramming;
 
-var calculator = new ShapeMeasurementsCalculator();
+var appointment = new MedicalAppointment("John Doe", new DateTime(2026, 10, 1));
 
-var rectangle1 = new Rectangle(5, 10);
-Console.WriteLine($"Rectangle width is {rectangle1.Width}");
-Console.WriteLine($"Rectangle height is {rectangle1.Height}");
-Console.WriteLine($"Rectangle circumference {calculator.CalculateRectangleCircumference(rectangle1)}");
-Console.WriteLine($"Rectangle area {calculator.CalculateRectangleArea(rectangle1)}");
-
-var rectangle2 = new Rectangle(40, 1);
-Console.WriteLine($"Rectangle width is {rectangle2.Width}");
-Console.WriteLine($"Rectangle height is {rectangle2.Height}");
-Console.WriteLine($"Rectangle circumference {calculator.CalculateRectangleCircumference(rectangle2)}");
-Console.WriteLine($"Rectangle area {calculator.CalculateRectangleArea(rectangle2)}");
-
-var triangle = new Triangle(10, 5);
-Console.WriteLine(triangle.CalculateArea());
-Console.WriteLine(triangle.AsString());
+appointment.OverwriteMonthAndDay(5, 1);
+appointment.MoveByMonthAndDays(1, 2); 
