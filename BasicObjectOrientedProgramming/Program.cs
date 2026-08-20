83912fe52dd4dc092731095f6a88e866a22963e8
alistkov <alistkov.dev@gmail.com>
2026-08-20 19:57:08 +0200

    video 90

--- BasicObjectOrientedProgramming/Program.cs
+++ BasicObjectOrientedProgramming/Program.cs
@@ -1,3 +1,24 @@
 ﻿using BasicObjectOrientedProgramming;
 
-Console.WriteLine(StringsTransformator.TransformSeparators("this,is,some,string", ",", "+"));
+var names = new Names();
+var path = names.BuildFilePath();
+
+if (File.Exists(path))
+{
+    Console.WriteLine("Names file already exists. Load names");
+    names.ReadFromTextFile();
+}
+else
+{
+    Console.WriteLine("Names file does not yet exist.");
+    
+    names.AddName("John");
+    names.AddName("not valid name");
+    names.AddName("Claire");
+    names.AddName("123 definitely not a valid name");
+    
+    Console.WriteLine("Saving names to a file.");
+    names.WriteToTextFile();
+}
+
+Console.WriteLine(names.Format());
