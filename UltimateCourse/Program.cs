e2851ea1b2a77177a418d5d155b0f69b511abf92
alistkov <alistkov.dev@gmail.com>
2026-08-22 00:06:30 +0200

    video 105

--- UltimateCourse/Program.cs
+++ UltimateCourse/Program.cs
@@ -1,2 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+public class Pizza
+{
+    private List<Ingredient> _ingredients = new List<Ingredient>();
+
+    public void AddIngredient(Ingredient ingredient)
+    {
+        _ingredients.Add(ingredient);
+    }
+
+    public string Describe() =>
+        $"This is a pizza with {string.Join(", ", _ingredients)}";
+}
+
+public class Ingredient
+{}
+
+public class Cheddar : Ingredient
+{
+    public string Name => "Cheddar cheese";
+    public int AgedForMonths { get; }
+}
+
+public class TomatoSauce : Ingredient
+{
+    public string Name => "Tomato sauce";
+    public int TomatoesIn100Grams { get; }
+}
+
+public class Mozzarella : Ingredient
+{
+    public string Name => "Mozzarella";
+    public int AgedForMonths { get; }
+}
