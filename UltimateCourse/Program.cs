7d3b51809f75d716d944936279fda448d37e8bc4
alistkov <alistkov.dev@gmail.com>
2026-08-22 18:24:55 +0200

    video 107

--- UltimateCourse/Program.cs
+++ UltimateCourse/Program.cs
@@ -1,9 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Console.WriteLine("Variable of type Cheddar");
+// var cheddar = new Cheddar();
+// Console.WriteLine(cheddar.Name);
+//
+// Console.WriteLine("Variable of type Ingredient");
+// Ingredient ingredient = new Cheddar();
+// Console.WriteLine(ingredient.Name);
 
-var chedar = new Cheddar();
-Console.WriteLine(chedar.PublicMethods());
+var ingredients = new List<Ingredient>
+{
+    new Cheddar(),
+    new TomatoSauce(),
+    new Mozzarella()
+};
 
+foreach (var ingredient in ingredients)
+{
+    Console.WriteLine(ingredient.Name);
+}
 
 public class Pizza
 {
@@ -20,30 +33,37 @@ public class Pizza
 
 public class Ingredient
 {
+    public virtual string Name { get; } = "Some ingredient";
+
+    public int PublicField;
+
     public string PublicMethods() =>
         "This method is PUBLIC in the Ingredient class";
-    
+
     private string PrivateMethods() =>
         "This method is PRIVATE in the Ingredient class";
-    
+
     protected string ProtectedMethods() =>
         "This method is PROTECTED in the Ingredient class";
-}   
+}
 
 public class Cheddar : Ingredient
 {
-    public string Name => "Cheddar cheese";
+    public override string Name => "Cheddar cheese";
+
     public int AgedForMonths { get; }
 }
 
 public class TomatoSauce : Ingredient
 {
-    public string Name => "Tomato sauce";
+    public override string Name => "Tomato sauce";
+
     public int TomatoesIn100Grams { get; }
 }
 
 public class Mozzarella : Ingredient
 {
-    public string Name => "Mozzarella";
-    public int AgedForMonths { get; }
+    public override string Name => "Mozzarella";
+
+    public bool IsLight { get; }
 }
