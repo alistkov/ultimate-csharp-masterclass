22c19abf4b779ad0cd69161e225c840498d389fa
alistkov <alistkov.dev@gmail.com>
2026-08-23 23:59:32 +0200

    video 128

--- UltimateCourse/Program.cs
+++ UltimateCourse/Program.cs
@@ -1,4 +1,18 @@
-﻿using UltimateCourse;
+﻿using System.Text.Json;
+using UltimateCourse;
 
-var result = Exercise.Transform(4);
-Console.WriteLine(result);
+var person = new Person
+{
+    FirstName = "John",
+    LastName = "Doe",
+    YearOfBirth = 1980
+};
+var asJson = JsonSerializer.Serialize(person);
+Console.WriteLine($"As JSON: {asJson}");
+
+var personJson = 
+    "{\"FirstName\":\"John\",\"LastName\":\"Doe\",\"YearOfBirth\":1980}";
+
+var personFromJson = JsonSerializer.Deserialize<Person>(personJson);
+
+Console.ReadKey();
