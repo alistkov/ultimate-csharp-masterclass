04ef21db60ad778c89454113500b08758c5c5004
alistkov <alistkov.dev@gmail.com>
2026-08-25 23:35:48 +0200

    video 146

--- UltimateCourse/Program.cs
+++ UltimateCourse/Program.cs
@@ -1,31 +1,19 @@
-﻿Console.Write("Enter a number: ");
-var userInput = Console.ReadLine();
-
-try
+﻿try
 {
-    var number = ParseStringToInt(userInput);
-    var result = 10 / number;
-    // Console.WriteLine($"String successfully parsed, the result is {number}");
-    Console.WriteLine($"10 / {number} is {result}");
-}
-catch (FormatException ex)
-{
-    Console.WriteLine("Wrong format. Input string is not parsable to int. Exception message: " + ex.Message);
-}
-catch (DivideByZeroException ex)
-{
-    Console.WriteLine("Diviision by zero is an invalid operation. Exception message: " + ex.Message);
+    var result = GetFirstElement(Array.Empty<int>());
 }
 catch (Exception ex)
 {
-    Console.WriteLine("Unexpected error occurred. Exception message: " + ex.Message);
-}
-finally
-{
-    Console.WriteLine("Finally block is being executed");
+    Console.WriteLine(ex.Message);
 }
 
-int ParseStringToInt(string input)
+
+int GetFirstElement(IEnumerable<int> numbers)
 {
-    return int.Parse(input);
+    foreach (var number in numbers)
+    {
+        return number;
+    }
+
+    throw new Exception("The collection cannot be empty");
 }
