64702d0b772509450275bccefbdc795de2cd29bb
alistkov <alistkov.dev@gmail.com>
2026-08-25 23:27:02 +0200

    video 145

--- UltimateCourse/Program.cs
+++ UltimateCourse/Program.cs
@@ -1,4 +1,31 @@
-﻿using UltimateCourse;
+﻿Console.Write("Enter a number: ");
+var userInput = Console.ReadLine();
 
-ExceptionsDivisionExercise.DivideNumbers(2, 4);
-ExceptionsDivisionExercise.DivideNumbers(2, 0);
+try
+{
+    var number = ParseStringToInt(userInput);
+    var result = 10 / number;
+    // Console.WriteLine($"String successfully parsed, the result is {number}");
+    Console.WriteLine($"10 / {number} is {result}");
+}
+catch (FormatException ex)
+{
+    Console.WriteLine("Wrong format. Input string is not parsable to int. Exception message: " + ex.Message);
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine("Diviision by zero is an invalid operation. Exception message: " + ex.Message);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Unexpected error occurred. Exception message: " + ex.Message);
+}
+finally
+{
+    Console.WriteLine("Finally block is being executed");
+}
+
+int ParseStringToInt(string input)
+{
+    return int.Parse(input);
+}
