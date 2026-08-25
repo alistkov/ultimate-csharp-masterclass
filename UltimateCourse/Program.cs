ca6a406d7af5b18a8368880d5f0c62387625dba5
alistkov <alistkov.dev@gmail.com>
2026-08-25 23:12:02 +0200

    video 144

--- UltimateCourse/Program.cs
+++ UltimateCourse/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter a number: ");
+var input = Console.ReadLine();
+
+try
+{
+    var number = ParseStringToInt(input);
+    Console.WriteLine($"String successfully parsed, the result is {number}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("An exception was thrown " + ex.Message);
+}
+finally
+{
+    Console.WriteLine("Finally block is being executed");
+}
+
+int ParseStringToInt(string input)
+{
+    return int.Parse(input);
+}
