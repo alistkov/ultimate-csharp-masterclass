79643f9d85c461cb25b41be71cef5a21f5a2279b
alistkov <alistkov.dev@gmail.com>
2026-08-26 22:29:04 +0200

    video 164

--- GameDataParser/Program.cs
+++ GameDataParser/Program.cs
@@ -1,11 +1,35 @@
 ﻿using System.Text.Json;
 
-Console.Write("Enter the name of the file you want to read: ");
-var fileName = Console.ReadLine();
-var fileContent = File.ReadAllText(fileName);
+bool isFileRead = false;
+var fileContent = default(string);
+do
+{
+    try
+    {
+        Console.WriteLine("Enter the name of the file you want to read: ");
+        var fileName = Console.ReadLine();
+        fileContent = File.ReadAllText(fileName);
+        isFileRead = true;
+    }
+    catch (ArgumentNullException ex)
+    {
+        Console.WriteLine("The filename cannot be null");
+    }
+    catch (ArgumentException ex)
+    {
+        Console.WriteLine("The filename cannot be empty");
+    }
+    catch (FileNotFoundException ex)
+    {
+        Console.WriteLine("The file does not exist");
+    }
+} while (!isFileRead);
+
 var videoGames = JsonSerializer.Deserialize<List<VideoGame>>(fileContent);
 
-if (videoGames.Count > 0)
+
+
+if (videoGames is not null && videoGames.Count > 0)
 {
     Console.WriteLine();
     Console.WriteLine("Loaded games are:");
