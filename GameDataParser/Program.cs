cc30aabbb4994f0087a42e3ebb5692eeefb78b91
alistkov <alistkov.dev@gmail.com>
2026-08-26 22:13:03 +0200

    video 163

---
+++ GameDataParser/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+Console.Write("Enter the name of the file you want to read: ");
+var fileName = Console.ReadLine();
+var fileContent = File.ReadAllText(fileName);
+var videoGames = JsonSerializer.Deserialize<List<VideoGame>>(fileContent);
+
+if (videoGames.Count > 0)
+{
+    Console.WriteLine();
+    Console.WriteLine("Loaded games are:");
+    foreach (var videoGame in videoGames)
+    {
+        Console.WriteLine(videoGame);
+    }
+}
+
+public class VideoGame
+{
+    public string Title { get; init; }
+    public int ReleaseYear { get; init; }
+    public decimal Rating { get; init; }
+
+    public override string ToString() =>
+        $"{Title}, released in {ReleaseYear}, rating: {Rating}";
+}
