c1dd6a2337b740d0c274c6a4f7eacbafb491a046
alistkov <alistkov.dev@gmail.com>
2026-08-27 21:50:04 +0200

    video 174

--- UltimateCourse/Program.cs
+++ UltimateCourse/Program.cs
@@ -6,6 +6,8 @@ numbers.Add(30);
 numbers.Add(40);
 numbers.Add(50);
 
+numbers.RemoveAt(2);
+
 Console.ReadKey();
 
 public class SimplifiedList
@@ -28,5 +30,32 @@ public class SimplifiedList
 
         _items[_size] = item;
         _size += 1;
+    }
+
+    public void RemoveAt(int index)
+    {
+        if (index < 0 || index > _size)
+        {
+            throw new ArgumentOutOfRangeException($"Index {index} outside the bounds of the list");
+        }
+
+        _size -= 1;
+
+        for (int i = index; i < _size; i += 1)
+        {
+            _items[i] = _items[i + 1];
+        }
+
+        _items[_size] = 0;
+    }
+
+    public int GetAtIndex(int index)
+    {
+        if (index < 0 || index >= _size)
+        {
+            throw new ArgumentOutOfRangeException($"Index {index} outside the bounds of the list");
+        }
+
+        return _items[index];
     }
 }
