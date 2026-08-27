81d19fee4624ceedff509f88ae3ba214b85d96dd
alistkov <alistkov.dev@gmail.com>
2026-08-27 21:42:15 +0200

    video 173

--- UltimateCourse/Program.cs
+++ UltimateCourse/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var numbers = new SimplifiedList();
+
+numbers.Add(10);
+numbers.Add(20);
+numbers.Add(30);
+numbers.Add(40);
+numbers.Add(50);
+
+Console.ReadKey();
+
+public class SimplifiedList
+{
+    private int[] _items = new int[4];
+    private int _size = 0;
+
+    public void Add(int item)
+    {
+        if (_size >= _items.Length)
+        {
+            var items = new int[_items.Length * 2];
+            for (int i = 0; i < _items.Length; i++)
+            {
+                items[i] = _items[i];
+            }
+
+            _items = items;
+        }
+
+        _items[_size] = item;
+        _size += 1;
+    }
+}
