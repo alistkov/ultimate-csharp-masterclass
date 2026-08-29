3f0daab8855cf46c7c9f2b5a84a461e91056b19f
alistkov <alistkov.dev@gmail.com>
2026-08-29 23:28:30 +0200

    generic methods exercise

--- UltimateCourse/Program.cs
+++ UltimateCourse/Program.cs
@@ -1,25 +1,5 @@
-﻿var decimals = new List<decimal> { 1.1m, 0.5m, 22.4m, 12.9m };
-var ints = decimals.ConvertTo<decimal, int>();
+﻿using UltimateCourse;
 
-Console.ReadKey();
-
-public static class ListExtensions
-{
-    public static void AddToFront<T>(this List<T> list, T item)
-    {
-        list.Insert(0, item);
-    }
-
-    public static List<TTarget> ConvertTo<TSource, TTarget>(this List<TSource> decimals)
-    {
-        var result = new List<TTarget>();
-
-        foreach (var item in decimals)
-        {
-            TTarget itemAfterCasting = (TTarget)Convert.ChangeType(item, typeof(TTarget))!;
-            result.Add(itemAfterCasting);
-        }
-
-        return result;
-    }
-}
+var tuple = new Tuple<int, string>(1, "hello");
+var swapped = TupleSwapExercise.SwapTupleItems(tuple);
+Console.WriteLine(swapped);
