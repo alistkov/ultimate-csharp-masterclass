c2b4c5c10f2c76902f96787a35102ce25daf13fa
alistkov <alistkov.dev@gmail.com>
2026-08-30 00:36:38 +0200

    video 182

--- UltimateCourse/Program.cs
+++ UltimateCourse/Program.cs
@@ -1,5 +1,30 @@
-﻿using UltimateCourse;
+﻿// var point = CreateCollectionOfRandomLength<Point>(100);
+// var numbers = CreateCollectionOfRandomLength<int>(100);
+var dates = CreateCollectionOfRandomLength<DateTime>(100);
 
-var tuple = new Tuple<int, string>(1, "hello");
-var swapped = TupleSwapExercise.SwapTupleItems(tuple);
-Console.WriteLine(swapped);
+Console.ReadKey();
+
+IEnumerable<T> CreateCollectionOfRandomLength<T>(int maxLength) where T: new()
+{
+    var length = new Random().Next(maxLength + 1);
+    var result = new List<T>();
+
+    for (int i = 0; i < length; i++)
+    {
+        result.Add(new T());
+    }
+
+    return result;
+}
+
+public class Point
+{
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+    
+    public int X { get; }
+    public int Y { get; }
+}
