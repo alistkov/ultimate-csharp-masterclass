299b8a1c573b35b910987eebc920842900f17a84
alistkov <alistkov.dev@gmail.com>
2026-08-29 23:15:00 +0200

    video 181

--- UltimateCourse/Program.cs
+++ UltimateCourse/Program.cs
@@ -1,16 +1,25 @@
-﻿var ints = new List<int> { 1, 2, 3 };
-ints.AddToFront(10);
-ints.AddToFront(11);
+﻿var decimals = new List<decimal> { 1.1m, 0.5m, 22.4m, 12.9m };
+var ints = decimals.ConvertTo<decimal, int>();
 
-foreach (var number in ints)
-{
-    Console.WriteLine(number);
-}
+Console.ReadKey();
 
 public static class ListExtensions
 {
     public static void AddToFront<T>(this List<T> list, T item)
     {
         list.Insert(0, item);
+    }
+
+    public static List<TTarget> ConvertTo<TSource, TTarget>(this List<TSource> decimals)
+    {
+        var result = new List<TTarget>();
+
+        foreach (var item in decimals)
+        {
+            TTarget itemAfterCasting = (TTarget)Convert.ChangeType(item, typeof(TTarget))!;
+            result.Add(itemAfterCasting);
+        }
+
+        return result;
     }
 }
