cf7a19cfc0cefd57cd2930b7f4ef2dec5cf59ada
alistkov <alistkov.dev@gmail.com>
2026-08-29 23:00:27 +0200

    video 179

--- UltimateCourse/Program.cs
+++ UltimateCourse/Program.cs
@@ -1,4 +1,16 @@
-﻿using System.Collections;
+﻿var ints = new List<int> { 1, 2, 3 };
+ints.AddToFront(10);
+ints.AddToFront(11);
 
-ArrayList numbers = new ArrayList { 1, 2, 3, 4 };
-ArrayList strings = new ArrayList { "a", "b", "c" };
+foreach (var number in ints)
+{
+    Console.WriteLine(number);
+}
+
+public static class ListExtensions
+{
+    public static void AddToFront<T>(this List<T> list, T item)
+    {
+        list.Insert(0, item);
+    }
+}
