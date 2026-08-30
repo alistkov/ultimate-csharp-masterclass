fc67ac83cfe51eb952b011517fc4468728334824
alistkov <alistkov.dev@gmail.com>
2026-08-30 23:07:34 +0200

    lambda exercisw

--- UltimateCourse/Exercise.cs
+++ UltimateCourse/Exercise.cs
@@ -2,14 +2,6 @@ namespace UltimateCourse;
 
 public class Exercise
 {
-    public void TestMethod()
-    {
-        Func<int, bool, double> someMethod1 = Method1;
-        Func<DateTime> someMethod2 = Method2;
-        Action<string, string> someMethod3 = Method3;
-    }
-        
-    public double Method1(int a, bool b) => 0;
-    public DateTime Method2() => default(DateTime);
-    public void Method3(string a, string b) {}
+    public Func<string, int> GetLength = word => word.Length;
+    public Func<int> GetRandomNumberBetween1And10 = () => new Random().Next(1, 11);
 }
