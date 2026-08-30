ff37d3dfe3ed2a9b5078504e2e6bbc967e0114d6
alistkov <alistkov.dev@gmail.com>
2026-08-31 00:42:59 +0200

    dictionaries exercise

--- UltimateCourse/Exercise.cs
+++ UltimateCourse/Exercise.cs
@@ -1,7 +1,40 @@
 namespace UltimateCourse;
 
-public class Exercise
+public static class Exercise
 {
-    public Func<string, int> GetLength = word => word.Length;
-    public Func<int> GetRandomNumberBetween1And10 = () => new Random().Next(1, 11);
+    public static Dictionary<PetType, double> FindMaxWeights(List<Pet> pets)
+    {
+        //your code goes here
+        var result = new Dictionary<PetType, double>();
+        
+        foreach (var pet in pets)
+        {
+            if (result.ContainsKey(pet.PetType))
+            {
+                result[pet.PetType] = result[pet.PetType] > pet.Weight ? result[pet.PetType] : pet.Weight;
+            }
+            else
+            {
+                result.Add(pet.PetType, pet.Weight);
+            }
+        }
+
+        return result;
+    }
 }
+    
+public class Pet
+{
+    public PetType PetType { get; }
+    public double Weight { get; }
+    
+    public Pet(PetType petType, double weight)
+    {
+        PetType = petType;
+        Weight = weight;    
+    }
+        
+    public override string ToString() => $"{PetType}, {Weight} kilos";
+}
+    
+public enum PetType { Dog, Cat, Fish }
--- UltimateCourse/Program.cs
+++ UltimateCourse/Program.cs
@@ -1,53 +1,15 @@
-﻿var employees = new List<Employee>
+﻿using UltimateCourse;
+
+var pets = new List<Pet>
 {
-    new Employee("Piter Parker", "Space navigation", 25000),
-    new Employee("Steve Rodgers", "Space navigation", 29800),
-    new Employee("Toni Stark", "Xenobiology", 21500),
-    new Employee("Bruce Banner", "Xenobiology", 22000),
-    new Employee("Natasha Romanov", "Mechanics", 21000),
-    new Employee("Thor", "Mechanics", 20000),
+    new Pet(PetType.Dog, 10),
+    new Pet(PetType.Cat, 5),
+    new Pet(PetType.Fish, 0.9),
+    new Pet(PetType.Dog, 45),
+    new Pet(PetType.Cat, 2),
+    new Pet(PetType.Fish, 0.02),
 };
 
-var result = CalculateAverageSalaryPerDepartment(employees);
-Console.WriteLine(result);
 
-Dictionary<string, decimal> CalculateAverageSalaryPerDepartment(IEnumerable<Employee> employees)
-{
-    var employeesPerDepartments = new Dictionary<string, List<Employee>>();
-
-    foreach (var employee in employees)
-    {
-        var employeeDepartment = employee.Department;
-        if (!employeesPerDepartments.ContainsKey(employeeDepartment))
-        {
-            employeesPerDepartments[employeeDepartment] = new List<Employee>();
-        }
-
-        employeesPerDepartments[employeeDepartment].Add(employee);
-    }
-
-    var averageSalaryByDepartment = new Dictionary<string, decimal>();
-
-    foreach (var employeesPerDepartment in employeesPerDepartments)
-    {
-        decimal sumOfDepartment = 0;
-
-        foreach (var employee in employeesPerDepartment.Value)
-        {
-            sumOfDepartment += employee.MonthlySalary;
-        }
-
-        var average = sumOfDepartment / employeesPerDepartment.Value.Count;
-
-        averageSalaryByDepartment[employeesPerDepartment.Key] = average;
-    }
-
-    return averageSalaryByDepartment;
-}
-
-public class Employee(string name, string department, decimal monthlySalary)
-{
-    public string Name { get; init; } = name;
-    public string Department { get; init; } = department;
-    public decimal MonthlySalary { get; init; } = monthlySalary;
-}
+var t = Exercise.FindMaxWeights(pets);
+Console.ReadKey();
