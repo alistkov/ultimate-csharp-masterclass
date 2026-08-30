29ec46a643bab807cbf0d2b52fbf044eee7072f4
alistkov <alistkov.dev@gmail.com>
2026-08-30 12:05:48 +0200

    video 183

--- UltimateCourse/Program.cs
+++ UltimateCourse/Program.cs
@@ -1,15 +1,22 @@
 ﻿// var point = CreateCollectionOfRandomLength<Point>(100);
 // var numbers = CreateCollectionOfRandomLength<int>(100);
-var dates = CreateCollectionOfRandomLength<DateTime>(100);
+
+using System.Diagnostics;
+
+var stopWatch = Stopwatch.StartNew();
+var dates = CreateCollectionOfRandomLength<DateTime>(0);
+stopWatch.Stop();
+
+Console.WriteLine($"Execution took {stopWatch.ElapsedMilliseconds} ms");
 
 Console.ReadKey();
 
 IEnumerable<T> CreateCollectionOfRandomLength<T>(int maxLength) where T: new()
 {
-    var length = new Random().Next(maxLength + 1);
-    var result = new List<T>();
+    var length = 100000000; // new Random().Next(maxLength + 1);
+    var result = new List<T>(length);
 
-    for (int i = 0; i < length; i++)
+    for (var i = 0; i < length; i++)
     {
         result.Add(new T());
     }
