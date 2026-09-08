5784f0bbf73ac48d8ea4a3583b8c1b15bde98a56
alistkov <alistkov.dev@gmail.com>
2026-09-08 17:48:04 +0200

    video 199

--- UltimateCourse/Program.cs
+++ UltimateCourse/Program.cs
@@ -1,59 +1,22 @@
-﻿var numbers = new List<int> { 10, 12, -100, 55, 17, 22 };
+﻿var dataDownloader = new SlowDataDownloader();
 
-var filteringStrategySelector = new FilteringStrategySelector();
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id2"));
+Console.WriteLine(dataDownloader.DownloadData("id3"));
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id3"));
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id2"));
 
-Console.WriteLine("Select filter:");
-Console.WriteLine(string.Join(Environment.NewLine, filteringStrategySelector.FilteringStrategiesName));
-
-var userInput = Console.ReadLine();
-
-var filteringStrategy = filteringStrategySelector.Select(userInput);
-var result = new Filter().FilterBy(filteringStrategy, numbers);
-
-Print(result);
-
-void Print(IEnumerable<int> numbers)
+public interface IDataDownloader
 {
-    Console.WriteLine(string.Join(", ", numbers));
+    string DownloadData(string recourseId);
 }
 
-public class Filter
+public class SlowDataDownloader : IDataDownloader
 {
-    public IEnumerable<T> FilterBy<T>(Func<T, bool> predicate, IEnumerable<T> data)
+    public string DownloadData(string recourseId)
     {
-        var result = new List<T>();
-
-        foreach (var element in data)
-        {
-            if (predicate(element))
-            {
-                result.Add(element);
-            }
-        }
-
-        return result;
-    }
-}
-
-public class FilteringStrategySelector
-{
-    private readonly Dictionary<string, Func<int, bool>> _filteringStrategies = new Dictionary<string, Func<int, bool>>
-    {
-        ["Even"] = (number) => number % 2 == 0,
-        ["Odd"] = (number) => number % 2 != 0,
-        ["Positive"] = (number) => number > 0,
-        ["Negative"] = (number) => number < 0,
-    };
-
-    public IEnumerable<string> FilteringStrategiesName => _filteringStrategies.Keys;
-
-    public Func<int, bool> Select(string filterType)
-    {
-        if (!_filteringStrategies.ContainsKey(filterType))
-        {
-            throw new NotSupportedException($"{filterType} is not a valid filter");
-        }
-
-        return _filteringStrategies[filterType];
+        return $"Some data for id {recourseId}";
     }
 }
