498a055411dae65e24c17050e0ef103c65b552c8
alistkov <alistkov.dev@gmail.com>
2026-09-08 18:02:14 +0200

    video 202

--- UltimateCourse/Program.cs
+++ UltimateCourse/Program.cs
@@ -13,10 +13,34 @@ public interface IDataDownloader
     string DownloadData(string recourseId);
 }
 
+public class Cache<TKey, TData> where TKey : notnull
+{
+    private readonly Dictionary<TKey, TData> _cachedData = new();
+
+    public TData Get(TKey key, Func<TKey, TData> getForTheFirstTime)
+    {
+        if (!_cachedData.ContainsKey(key))
+        {
+            _cachedData[key] = getForTheFirstTime(key);
+        }
+
+        return _cachedData[key];
+    }
+}
+
 public class SlowDataDownloader : IDataDownloader
 {
-    public string DownloadData(string recourseId)
+    private readonly Cache<string, string> _cache = new();
+
+    public string DownloadData(string resourceId)
     {
-        return $"Some data for id {recourseId}";
+        return _cache.Get(resourceId, DownloadDataWithoutCaching);
+    }
+    
+    
+    private string DownloadDataWithoutCaching(string resourceId)
+    {
+        Thread.Sleep(1000);
+        return $"Some data for {resourceId}";
     }
 }
