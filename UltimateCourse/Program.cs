78310d087b586a78f46e2574a9faae74bb796926
alistkov <alistkov.dev@gmail.com>
2026-09-08 18:26:10 +0200

    remove code from Programm file

--- UltimateCourse/Program.cs
+++ UltimateCourse/Program.cs
@@ -1,13 +1,1 @@
-﻿using UltimateCourse.GenericTypes;
-
-var dataDownloader = new CachingDataDownloader(
-    new PrintingDataDownloader(
-        new SlowDataDownloader()));
-
-Console.WriteLine(dataDownloader.DownloadData("id1"));
-Console.WriteLine(dataDownloader.DownloadData("id2"));
-Console.WriteLine(dataDownloader.DownloadData("id3"));
-Console.WriteLine(dataDownloader.DownloadData("id1"));
-Console.WriteLine(dataDownloader.DownloadData("id3"));
-Console.WriteLine(dataDownloader.DownloadData("id1"));
-Console.WriteLine(dataDownloader.DownloadData("id2"));
+﻿Console.WriteLine("Hello, World!");
