942ae4cac6573323f0215a278af351d9e22bc207
alistkov <alistkov.dev@gmail.com>
2026-09-10 23:51:22 +0200

    video 235

--- UltimateCourse/Program.cs
+++ UltimateCourse/Program.cs
@@ -1,33 +1,19 @@
 ﻿var number = 5;
-var person = new Person { Name = "John Doe", Age = 35 };
 
-AddOneToNumber(number);
-AddOneToPersonAge(person);
-
+AddOneToNumber(ref number);
 Console.WriteLine($"number now is {number}");
-Console.WriteLine($"person age now is {person.Age}");
 
-void AddOneToNumber(int number)
+var otherNumber = 19;
+
+MethodWithOutParameter(out otherNumber);
+Console.WriteLine($"other number is {otherNumber}");
+
+void MethodWithOutParameter(out int number)
+{
+    number = 10;
+}
+
+void AddOneToNumber(ref int number)
 {
     ++number;
-}
-
-// void AddOneToPersonAge(Person person)
-// {
-//     ++person.Age;
-// }
-
-Person AddOneToPersonAge(Person person)
-{
-    return new Person
-    {
-        Name = person.Name,
-        Age = person.Age + 1
-    };
-}
-
-struct Person
-{
-    public required string Name { get; init; }
-    public required int Age { get; init; }
 }
