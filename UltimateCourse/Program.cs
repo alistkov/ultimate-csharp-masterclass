99fdb4ad67a532804c8bb75dd2d4f0fc0d3dc9d1
alistkov <alistkov.dev@gmail.com>
2026-09-10 19:26:44 +0200

    clean Programm file

--- UltimateCourse/Program.cs
+++ UltimateCourse/Program.cs
@@ -1,13 +1,1 @@
-﻿using UltimateCourse.Linq;
-
-// LinqTutorial.Run();
-
-var timeSpans = new List<TimeSpan>
-{
-    new TimeSpan(0, 0, 0, 0, 100),
-    new TimeSpan(0, 0, 0, 0, 50),
-    new TimeSpan(0, 0, 0, 0, 30),
-};
-
-var t = Exercise.CalculateAverageDurationInMilliseconds(timeSpans);
-Console.WriteLine(t);
+﻿Console.WriteLine("Hello, World!");
