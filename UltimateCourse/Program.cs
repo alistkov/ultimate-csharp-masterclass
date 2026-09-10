219ec384deacf99cace6d893ca285a91973f405a
alistkov <alistkov.dev@gmail.com>
2026-09-10 23:37:22 +0200

    video 233

--- UltimateCourse/Program.cs
+++ UltimateCourse/Program.cs
@@ -1,19 +1,24 @@
 ﻿var number = 5;
-var anotherNumber = number;
-anotherNumber++;
-
 var person = new Person { Name = "John Doe", Age = 35 };
-var copyPerson = person;
-copyPerson.Name = "Sam Smith";
 
-Console.WriteLine($"number is {number}");
-Console.WriteLine($"another number is {anotherNumber}");
-Console.WriteLine("---------------------");
-Console.WriteLine($"Person name {person.Name}");
-Console.WriteLine($"Copy person name {copyPerson.Name}");
+AddOneToNumber(number);
+AddOneToPersonAge(person);
+
+Console.WriteLine($"number now is {number}");
+Console.WriteLine($"person age now is {person.Age}");
+
+void AddOneToNumber(int number)
+{
+    ++number;
+}
+
+void AddOneToPersonAge(Person person)
+{
+    ++person.Age;
+}
 
 class Person
 {
-    public string Name { get; set; }
-    public int Age { get; set; }
+    public required string Name { get; set; }
+    public required int Age { get; set; }
 }
