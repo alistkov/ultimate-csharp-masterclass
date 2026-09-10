f682ffec87618dbdee68c7c0c5d6daacaa4bbbe9
alistkov <alistkov.dev@gmail.com>
2026-09-10 23:28:35 +0200

    video 232

--- UltimateCourse/Program.cs
+++ UltimateCourse/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var number = 5;
+var anotherNumber = number;
+anotherNumber++;
+
+var person = new Person { Name = "John Doe", Age = 35 };
+var copyPerson = person;
+copyPerson.Name = "Sam Smith";
+
+Console.WriteLine($"number is {number}");
+Console.WriteLine($"another number is {anotherNumber}");
+Console.WriteLine("---------------------");
+Console.WriteLine($"Person name {person.Name}");
+Console.WriteLine($"Copy person name {copyPerson.Name}");
+
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
