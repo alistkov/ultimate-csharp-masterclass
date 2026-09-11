8073db22aa7c15450247bc72ed89a17ed2d931e5
alistkov <alistkov.dev@gmail.com>
2026-09-11 14:30:26 +0200

    video 239

--- UltimateCourse/Program.cs
+++ UltimateCourse/Program.cs
@@ -1,7 +1,13 @@
-﻿int number = 5;
+﻿using System.Collections;
+
+int number = 5;
 var person = new Person { Name = "Sam", Age = 22 };
 object boxedNumber = number;
 short unboxedNumber = (short)boxedNumber;
+
+var numbers1 = new List<int> { 1, 2, 3, 4, 5 };
+var numbers2 = new ArrayList { 1, 2, 3, 4, 5 };
+var numbers3 = new List<IComparable<int>> { 1, 2, 3, 4, 5 };
 
 var variousObjects = new List<object>
 {
