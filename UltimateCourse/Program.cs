4d636b689d452e01e2b7f032844d5600d06cf905
alistkov <alistkov.dev@gmail.com>
2026-09-11 14:43:30 +0200

    video 240

--- UltimateCourse/Program.cs
+++ UltimateCourse/Program.cs
@@ -1,24 +1,11 @@
-﻿using System.Collections;
-
-int number = 5;
-var person = new Person { Name = "Sam", Age = 22 };
-object boxedNumber = number;
-short unboxedNumber = (short)boxedNumber;
-
-var numbers1 = new List<int> { 1, 2, 3, 4, 5 };
-var numbers2 = new ArrayList { 1, 2, 3, 4, 5 };
-var numbers3 = new List<IComparable<int>> { 1, 2, 3, 4, 5 };
-
-var variousObjects = new List<object>
+﻿string userInput = Console.ReadLine();
+if (userInput == "Print person")
 {
-    1, 3L, 1.5m, new DateTime(2027, 1, 1), "hello", new Person { Name = "John Doe", Age = 35 }, true
-};
-
-foreach (var someObject in variousObjects)
-{
-    Console.WriteLine(someObject);
-    Console.WriteLine(someObject.GetType().Name);
+    Person person = new Person { Name = "John Doe", Age = 35 };
+    Console.WriteLine($"{person.Name} is {person.Age} years old");
 }
+
+GC.Collect();
 
 Console.ReadKey();
 
