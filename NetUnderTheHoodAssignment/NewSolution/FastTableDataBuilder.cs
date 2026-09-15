69f80b932db536ad549cf44c30f081a00a008707
alistkov <alistkov.dev@gmail.com>
2026-09-15 20:10:21 +0200

    video 256

--- NetUnderTheHoodAssignment/NewSolution/FastTableDataBuilder.cs
+++ NetUnderTheHoodAssignment/NewSolution/FastTableDataBuilder.cs
@@ -12,49 +12,43 @@ public class FastTableDataBuilder : ITableDataBuilder
 
         foreach (var row in csvData.Rows)
         {
-            var newRowData = new Dictionary<string, object>();
+            var newRow = new FastRow();
 
             for (int columnIndex = 0; columnIndex < csvData.Columns.Length; ++columnIndex)
             {
                 var column = csvData.Columns[columnIndex];
                 string valueAsString = row[columnIndex];
-                object value = ConvertValueToTargetType(valueAsString);
-
-                if (value is not null)
+                
+                if (string.IsNullOrEmpty(valueAsString))
                 {
-                    newRowData[column] = value;
+                    continue;
+                }
+                if (valueAsString == "TRUE")
+                {
+                    newRow.AssignCell(column, true);
+                }
+                else if (valueAsString == "FALSE")
+                {
+                    newRow.AssignCell(column, false);
+                }
+                else if (valueAsString.Contains(".") && decimal.TryParse(valueAsString, out var valueAsDecimal))
+                {
+                    newRow.AssignCell(column, valueAsDecimal);
+                }
+                else if (int.TryParse(valueAsString, out var valueAsInt))
+                {
+                    newRow.AssignCell(column, valueAsInt);
+                }
+                else
+                {
+                    newRow.AssignCell(column, valueAsString);
                 }
             }
 
-            resultRows.Add(new FastRow(newRowData));
+            resultRows.Add(newRow);
         }
 
         return new FastTableData(csvData.Columns, resultRows);
-    }
-    
-    private object ConvertValueToTargetType(string value)
-    {
-        if (string.IsNullOrEmpty(value))
-        {
-            return null;
-        }
-        if (value == "TRUE")
-        {
-            return true;
-        }
-        if (value == "FALSE")
-        {
-            return false;
-        }
-        if (value.Contains(".") && decimal.TryParse(value, out var valueAsDecimal))
-        {
-            return valueAsDecimal;
-        }
-        if (int.TryParse(value, out var valueAsInt))
-        {
-            return valueAsInt;
-        }
-        return value;
     }
 }
 
